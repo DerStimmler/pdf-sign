87b6bc369c31e4ce1611f3ec6e989e45a91598de
Tim Schneider <43130816+DerStimmler@users.noreply.github.com>
2024-09-08 17:17:43 +0200

    refactor: move root styles to new file

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,5 @@
 <script lang="ts">
-  import '../app.css';
   import Canvas from './Canvas.svelte';
 </script>
 
-<main class="container mx-auto py-4 grid gap-4">
-  <h1
-    class="place-self-center text-6xl leading-[inherit] font-bold w-max m-0 text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500"
-  >
-    pdf-sign
-  </h1>
-  <p class="place-self-center opacity-80 mb-4">Easily sign your PDFs locally in your browser for maximum privacy.</p>
-  <Canvas></Canvas>
-</main>
+<Canvas></Canvas>
